9a4fc1281f0127bc8ac82f657501f8abd3b73f53
Kalyan Kanakatla <40964482+kalyanocjp@users.noreply.github.com>
2019-04-21 21:32:12 -0500

    Update Service2Controller.java

--- Service2/src/main/java/com/example/demo/Service2Controller.java
+++ Service2/src/main/java/com/example/demo/Service2Controller.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ public class Service2Controller {
 	
 	Logger log = LoggerFactory.getLogger(Service2Controller.class);
 	
+	@Value("${message: default Hello}")
+	private String message;
+	
 	@RequestMapping("/service2")
 	public String getService2Response() {
-		log.info("Inside 2nd response....");
+		log.info("Inside 2nd response...."+message);
 		return "Hello service2 here";
 	}
 
